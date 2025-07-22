92da75e0b986fe0d3d7960838288b2e1b6568595
aarogyaojha <arogyaojha251@gmail.com>
2025-07-22 15:18:50 +0545

    added text field widgets

---
+++ lib/presentation/widgets/shared/app_text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppTextField extends StatelessWidget {
+  final String label;
+  final String? hint;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final List<TextInputFormatter>? inputFormatters;
+  final String? Function(String?)? validator;
+  final Widget? suffixIcon;
+  final int maxLines;
+
+  const AppTextField({
+    Key? key,
+    required this.label,
+    this.hint,
+    this.controller,
+    this.keyboardType,
+    this.inputFormatters,
+    this.validator,
+    this.suffixIcon,
+    this.maxLines = 1,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      inputFormatters: inputFormatters,
+      validator: validator,
+      maxLines: maxLines,
+      decoration: InputDecoration(
+        labelText: label,
+        hintText: hint,
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+        contentPadding: const EdgeInsets.symmetric(
+          vertical: 16,
+          horizontal: 16,
+        ),
+      ),
+    );
+  }
+}
