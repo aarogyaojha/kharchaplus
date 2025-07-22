aae1f9f821f254dd1556dd944726a6e6989bed7d
aarogyaojha <arogyaojha251@gmail.com>
2025-07-22 15:14:45 +0545

    transit route model

---
+++ lib/domain/models/transit_route.dart
@@ -0,0 +1,17 @@
+class TransitRoute {
+  final String id;
+  final String name;
+  final String from;
+  final String to;
+  final double fare;
+  final String type;
+
+  TransitRoute({
+    required this.id,
+    required this.name,
+    required this.from,
+    required this.to,
+    required this.fare,
+    required this.type,
+  });
+}
